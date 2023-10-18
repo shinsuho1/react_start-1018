15a0b6731f0912438ab8f2d3e7beefdab5c89563
shinsuho1 <suhoshin1816@gmail.com>
2023-10-18 12:35:50 +0900

    index.html onload수정

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from './App';
 import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
-import { Store } from 'redux';
+// import { Store } from 'redux';
 
 ReactDOM.render(
 	<HashRouter>
